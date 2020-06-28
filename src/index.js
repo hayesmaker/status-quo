7f9830733238a2014895edc89da57eab7b91d2b0
hayesmaker <andygoestohollywood@gmail.com>
2020-06-28 20:02:14 +0100

    Safely create output folder if required

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@
 const fs = require('fs');
 const translate = require('translate');
 const Encoding = require('encoding-japanese');
+const mkdirp = require('mkdirp');
 require('dotenv').config();
 translate.from = 'ko';
 translate.engine = 'google';
 translate.key = process.env.GOOGLE_CLOUD_KEY;
 const FILE_PATH = './input/snakes-utf8.tsv';
-const OUTPUT_PATH = './output/snakes.txt';
+const OUTPUT_FOLDER = './output';
+const OUTPUT_PATH = OUTPUT_FOLDER + '/snakes.txt';
 const MAX_RESULTS = 5; //-1 for all results
 
 const fileBuffer = fs.readFileSync(FILE_PATH);
 console.log(Encoding.detect(fileBuffer));
+
+mkdirp(OUTPUT_FOLDER).then(made => {
+  if (made) {
+    console.log(`OUTPUT_FOLDER CREATED: ${made}`);
+  }
+});
 
 fs.readFile(FILE_PATH, 'utf8', async function (err, contents) {
 
@@ -26,7 +34,6 @@ fs.readFile(FILE_PATH, 'utf8', async function (err, contents) {
     });
     const outputData = [];
     let outputStr = "";
-
     const t = async data => {
         try {
             let res = await translate(data.text, {to: 'en'});
@@ -61,17 +68,15 @@ fs.readFile(FILE_PATH, 'utf8', async function (err, contents) {
     }
 
     const storeData = (data, path) => {
-        console.log("store", data);
         try {
             fs.writeFile(path, data, () => {
               if (err) return console.log(err);
               console.log("Output created successfully at" + OUTPUT_PATH);
             });
         } catch (err) {
-            console.error(err)
+            console.error(err);
         }
     };
-
     storeData(outputStr, OUTPUT_PATH);
 });
 
